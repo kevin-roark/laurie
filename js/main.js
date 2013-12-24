f69ca31c1ba2c9ca6188e97cfda40927fc1c89b4
Kevin Roark <ker2143@columbia.edu>
2013-12-23 20:48:18 -0600

    added the fancy

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function() {
 
+    // RequestAnimFrame: a browser API for getting smooth animations
+    window.requestAnimFrame = (function(){
+      return  window.requestAnimationFrame       || 
+              window.webkitRequestAnimationFrame || 
+              window.mozRequestAnimationFrame    || 
+              window.oRequestAnimationFrame      || 
+              window.msRequestAnimationFrame     ||  
+              function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+              };
+    })();
+
     var xmas = $('#chrimbas-text');
     var cur_color = 'red';
     var border_color = 'green';
@@ -11,6 +23,30 @@ $(function() {
         $('#easter'),
         $('#nyc')
     ];
+
+    var xmas_colors = [
+        'rgb(255, 0, 0)',
+        'rgb(225, 25, 25)',
+        'rgb(205, 40, 40)',
+        'rgb(200, 50, 50)',
+        'rgb(175, 60, 60)',
+        'rgb(25, 225, 25)',
+        'rgb(40, 205, 40)',
+        'rgb(50, 200, 50)',
+        'rgb(60, 175, 60)',
+        'rgb(0, 255, 0)',
+    ];
+    xmas_colors.sort(function() {return 0.5 - Math.random()});
+
+    var canvas = document.getElementById('full-screen-canvas');
+    var ctx = canvas.getContext('2d');
+    ctx.width = window.innerWidth;
+    ctx.height = window.innerHeight;
+
+    var globes = [];
+    for (var i=0; i<15; i++)
+        globes.push(new Globe(-10, -10));
+    var nextGlobeColorIdx = -1;
 
     function getRandomInt(min, max) {
       return Math.floor(Math.random() * (max - min + 1) + min);
@@ -37,12 +73,62 @@ $(function() {
 
             var cur_top = parseInt(bouncer.css('top'));
             var cur_left = parseInt(bouncer.css('left'));
+            var next_top = Math.max(0, cur_top + bounce_y);
+            var next_left = Math.max(0, cur_left + bounce_x);
 
-            bouncer.css('top', cur_top + bounce_y);
-            bouncer.css('left', cur_left + bounce_x);
+            bouncer.css('top', next_top);
+            bouncer.css('left', next_left);
         }
+    }
+
+    function clearCanvas() {
+        ctx.clearRect(0, 0, ctx.width, ctx.height);
+    }
+
+    function draw() {
+        clearCanvas();
+        for (var i=0; i<globes.length; i++) {
+            var globe = globes[i];
+            globe.draw();
+        }
+    }
+
+    function Globe(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = 10;
+        this.color = color;
+
+        this.draw = function() {
+            ctx.beginPath();
+            ctx.fillStyle = this.color;
+            ctx.arc(this.x, this.y, this.radius, Math.PI*2, false);
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+
+    function getNextGlobeColor() {
+        nextGlobeColorIdx++;
+        if (nextGlobeColorIdx >= xmas_colors.length)
+            nextGlobeColorIdx = 0;
+        return xmas_colors[nextGlobeColorIdx];
+    }
+
+    function mouse_moved(e) {
+        globes.shift();
+        var x = e.pageX;
+        var y = e.pageY;
+        globes.push(new Globe(x, y, getNextGlobeColor()));
+    }
+
+    function trailLoop() {
+        draw();
+        requestAnimFrame(trailLoop);
     }
 
     setInterval(change_xmas_color, 400);
     setInterval(bounce_images, 100);
+    $(document).mousemove(mouse_moved);
+    trailLoop();
 });
